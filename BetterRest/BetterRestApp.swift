9d024d310b349808c81cc60407e38e75be35f2c8
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-13 20:00:32 +0500

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Naveed on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
