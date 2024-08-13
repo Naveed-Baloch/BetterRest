7467fa64986c26b3c8bbea2fab3172a07dca4d86
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-13 20:30:53 +0500

    add basic stepper & Date Picker

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let dateRange = Date.now...Date.now.addingTimeInterval(86400) // 2nd range define the tomorrow in seconds
+    @State private var sleepAmount = 4.0
+    @State private var wakeUp = Date.now
     var body: some View {
         VStack {
+            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+            DatePicker("Please enter a date", selection: $wakeUp,in: dateRange)
+                .labelsHidden()
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
